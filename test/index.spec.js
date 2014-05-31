d173be6c3b80d8f40311bfa6a1f28614fe3c3946
Ryan Zec <code@ryanzec.com>
2014-05-31 13:55:19 -0400

    added support for property checking general refactoring

--- test/index.spec.js
+++ test/index.spec.js
@@ -12,81 +12,155 @@ interfaceChecker.define('MultipleMethods', [
   'two:1',
   'three:4'
 ]);
+interfaceChecker.define('WithProperty', [
+  'one:p',
+  'two',
+  'three:4'
+]);
 
 describe('interface-checker', function() {
-  it('validate method w/o parameter checking', function*() {
-    expect(interfaceChecker.has({
-      one: function() {
+  describe('methods', function() {
+    it('should validate w/o parameter checking', function*() {
+      expect(interfaceChecker.has({
+        one: function() {
 
-      }
-    }, 'NoParameters')).to.be.true;
-  });
+        }
+      }, 'NoParameters')).to.be.true;
+    });
 
-  it('validate method w/ parameter checking', function*() {
-    expect(interfaceChecker.has({
-      one: function(one, two) {
+    it('should validate w/ parameter checking', function*() {
+      expect(interfaceChecker.has({
+        one: function(one, two) {
 
-      }
-    }, 'Parameters')).to.be.true;
-  });
+        }
+      }, 'Parameters')).to.be.true;
+    });
 
-  it('validate method w/ multiple methods', function*() {
-    expect(interfaceChecker.has({
-      one: function() {
+    it('should validate w/ multiple methods', function*() {
+      expect(interfaceChecker.has({
+        one: function() {
 
-      },
-      two: function(one) {
+        },
+        two: function(one) {
 
-      },
-      three: function(one, two, three, four) {
+        },
+        three: function(one, two, three, four) {
 
-      }
-    }, 'MultipleMethods')).to.be.true;
-  });
+        }
+      }, 'MultipleMethods')).to.be.true;
+    });
 
-  it('should return error object when method is not satisfied', function*() {
-    expect(interfaceChecker.has({
-      two: function(one, two) {
+    it('should return error object when method is not satisfied', function*() {
+      expect(interfaceChecker.has({
+        two: function(one, two) {
 
-      }
-    }, 'Parameters')).to.deep.equal({
-      missing: [
-        'one'
-      ]
+        }
+      }, 'Parameters')).to.deep.equal({
+        missingMethods: [
+          'one'
+        ]
+      });
+    });
+
+    it('should return error object when parameter count is not satisfied', function*() {
+      expect(interfaceChecker.has({
+        one: function(one) {
+
+        }
+      }, 'Parameters')).to.deep.equal({
+        parameterMismatch: [
+          'one should have 2 parameters but it currently has 1 parameters'
+        ]
+      });
+    });
+
+    it('should return error object when multiple issues are present', function*() {
+      expect(interfaceChecker.has({
+        one: function(one) {
+
+        },
+        three: function(one, two) {
+
+        }
+      }, 'MultipleMethods')).to.deep.equal({
+        missingMethods: [
+          'two'
+        ],
+        parameterMismatch: [
+          'three should have 4 parameters but it currently has 2 parameters'
+        ]
+      });
+    });
+
+    it('should throw error when checking against interface that has not been defined', function*() {
+      var err = "There is not interface define as UndefinedInterface";
+
+      expect(function() {
+        interfaceChecker.has({}, 'UndefinedInterface');
+      }).to.throw(err);
+    });
+
+    it('should return error object when property is given', function*() {
+      expect(interfaceChecker.has({
+        one: 'test',
+        two: function(one) {
+
+        },
+        three: function(one, two, three, four) {
+
+        }
+      }, 'MultipleMethods')).to.deep.equal({
+        invalidType: {
+          one: 'method'
+        }
+      });
     });
   });
 
-  it('should return error object when method parameter count is not satisfied', function*() {
-    expect(interfaceChecker.has({
-      one: function(one) {
+  describe('properties', function() {
+    it('should validate property checking', function*() {
+      expect(interfaceChecker.has({
+        one: 'test',
+        two: function() {
 
-      }
-    }, 'Parameters')).to.deep.equal({
-      parameterMismatch: [
-        'one should have 2 parameters but it currently has 1 parameters'
-      ]
+        },
+        three: function(one, two, three, four) {
+
+        }
+      }, 'WithProperty')).to.be.true;
     });
-  });
 
-  it('should return error object when multiple issues are present', function*() {
-    expect(interfaceChecker.has({
-      one: function(one) {
+    it('should return error object w/ missing property', function*() {
+      expect(interfaceChecker.has({
+        two: function() {
 
-      },
-      three: function(one, two) {
+        },
+        three: function(one, two, three, four) {
 
-      }
-    }, 'MultipleMethods')).to.deep.equal({
-      missing: [
-        'two'
-      ],
-      parameterMismatch: [
-        'three should have 4 parameters but it currently has 2 parameters'
-      ]
+        }
+      }, 'WithProperty')).to.deep.equal({
+        missingProperties: [
+          'one'
+        ]
+      });
     });
-  });
 
-  it('should throw error when checking against interface that has not been defined', function*() {
+    it('should return error object when method is given', function*() {
+      expect(interfaceChecker.has({
+        one: function() {
 
+        },
+        two: function(one) {
+
+        },
+        three: function(one, two, three, four) {
+
+        }
+      }, 'WithProperty')).to.deep.equal({
+        invalidType: {
+          one: 'property'
+        }
+      });
+    });
   });
 });
